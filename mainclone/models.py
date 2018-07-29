24db55a6b7e4d2fe3744edf93d10467ca188bf19
Xavier Kibet <xavierkibet@gmail.com>
2018-07-29 22:37:24 +0300

    Add profile picture property by adding field in model

--- mainclone/models.py
+++ mainclone/models.py
@@ -7,6 +7,7 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     Name = models.TextField(default="Anonymous")
+    profile_picture = models.ImageField(upload_to='users/', default='users/user.png')
     bio = models.TextField(default="Welcome Me!")
 
     @receiver(post_save, sender=User)

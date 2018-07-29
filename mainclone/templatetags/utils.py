5902c36d564dae3a83d8fd1dcf18213f4c9f71ac
Xavier Kibet <xavierkibet@gmail.com>
2018-07-29 22:49:32 +0300

    Register custom template filter to check like on image

---
+++ mainclone/templatetags/utils.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+@register.filter(name='has_saved')
+def hsaved(user, photo):
+    for post in user.saves.all():
+        if photo == post.photo:
+            return True
+    return False

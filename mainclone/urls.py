8f07a449a94130fe1336c5b6a90b907eca63195c
Xavier Kibet <xavierkibet@gmail.com>
2018-07-29 22:52:42 +0300

    Add homepage and profile view function

--- mainclone/urls.py
+++ mainclone/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.home, name='landing')
-]
+    url(r'^$', views.home, name='landing'),
+    url(r'^myaccount/$', views.mine, name='myaccount'),
--- mainclone/views.py
+++ mainclone/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
+from .forms import *
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
 @login_required(login_url='/accounts/login/')
 def home(request):
-    Profile.usercheck(request.user)
+    image_form = PostForm()
+    images = Post.objects.all()
+    commentform = CommentForm()
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            request.user.profile.post(form)
     return render(request, 'landing.html', locals())
+
+
+@login_required(login_url='/accounts/login/')
+def mine(request):
+    images = request.user.profile.posts.all()
+    user_object = request.user
+    user_images = user_object.profile.posts.all()
+    user_saved = [save.photo for save in user_object.profile.saves.all()]
+    user_liked = [like.photo for like in user_object.profile.mylikes.all()]
+    print(user_liked)
+    return render(request, 'myprofile.html', locals())\

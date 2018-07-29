59347044fb51d964b9d2eef3058d3bb767130f22
Xavier Kibet <xavierkibet@gmail.com>
2018-07-29 23:41:25 +0300

    Update user interface to switch instgram logos on scroll

--- mainclone/static/js/index.js
+++ mainclone/static/js/index.js
@@ -3,4 +3,14 @@ sleep=(ms)=>{
 };
 async function nowsleep(x) {
   await sleep(x);
-}
+}}
+document.onscroll = ()=>
+    {
+        if (document.body.scrollTop > 100 ) {
+            $("#brand").removeClass("logo");
+            $("#brand").addClass("mainicon");
+        } else {
+            $("#brand").removeClass("mainicon");
+            $("#brand").addClass("logo");
+        }
+    };
